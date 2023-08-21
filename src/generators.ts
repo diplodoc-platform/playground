d62809e340a735ec0b733f318204e86f347741ab
moki <morozov.kirill.moki@gmail.com>
2023-08-21 23:06:24 +0400

    fix(generator): apply diplodoc plugins to md renderer

--- src/generators.ts
+++ src/generators.ts
@@ -1,16 +1,58 @@
 import MarkdownIt from 'markdown-it';
+
 // @ts-ignore
 import transform from '@doc-tools/transform';
 // @ts-ignore
-import md from '@doc-tools/transform/lib/md';
-import { mdRenderer } from '@diplodoc/markdown-it-markdown-renderer';
+import transformMarkdown from '@doc-tools/transform/lib/md';
+
+import {
+    mdRenderer,
+    MarkdownRendererParams,
+    MarkdownRendererEnv,
+    MarkdownRenderer,
+} from '@diplodoc/markdown-it-markdown-renderer';
+
+// @ts-ignore
+ import meta from 'markdown-it-meta';
+// @ts-ignore
+import sup from 'markdown-it-sup';
+import notes from '@doc-tools/transform/lib/plugins/notes';
+import cut from '@doc-tools/transform/lib/plugins/cut';
+import checkbox from '@doc-tools/transform/lib/plugins/checkbox';
+import anchors from '@doc-tools/transform/lib/plugins/anchors';
+import monospace from '@doc-tools/transform/lib/plugins/monospace';
+import imsize from '@doc-tools/transform/lib/plugins/imsize';
+import file from '@doc-tools/transform/lib/plugins/file';
+import includes from '@doc-tools/transform/lib/plugins/includes';
+import tabs from '@doc-tools/transform/lib/plugins/tabs';
+import video from '@doc-tools/transform/lib/plugins/video';
+import table from '@doc-tools/transform/lib/plugins/table';
+
+const diplodocOptions = {
+    lang: 'en',
+    path: '',
+};
 
 function generateMD(input: string) {
-  const mdit = new MarkdownIt({html: true});
-  mdit.use(mdRenderer);
+  const md = new MarkdownIt({html: true});
+
+  md.use(meta);
+  md.use(notes, diplodocOptions);
+  md.use(cut, diplodocOptions);
+  md.use(sup, diplodocOptions);
+  md.use(checkbox, diplodocOptions);
+  md.use(anchors, diplodocOptions);
+  md.use(monospace, diplodocOptions);
+  md.use(imsize, diplodocOptions);
+  md.use(file, diplodocOptions);
+  md.use(includes, diplodocOptions);
+  md.use(tabs, diplodocOptions);
+  md.use(video, diplodocOptions);
+  md.use(table, diplodocOptions);
+  md.use(mdRenderer);
 
   try {
-    return mdit.render(input, {source: input.split('\n')});
+    return md.render(input, {source: input.split('\n')});
   } catch(e) {
     console.error(e);
     return '';
@@ -27,7 +69,7 @@ function generateHTML(input: string) {
 }
 
 function generateTokens(input: string) {
-  const {parse} = md({});
+  const {parse} = transformMarkdown({});
 
   try {
     const tokens = parse(input);
