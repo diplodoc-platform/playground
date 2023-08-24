0a3ab963b8ccd2726664f5054b23245ab1868c51
moki <morozov.kirill.moki@gmail.com>
2023-08-24 12:23:57 +0400

    chore: indentation

---
+++ src/index.html.js
@@ -0,0 +1,38 @@
+function generateHTML({env, jspath, csspath} = {}) {
+    if (!env) {
+        env = 'development';
+    }
+
+    if (!jspath) {
+        jspath = 'index.js';
+    }
+
+    if (!csspath) {
+        csspath = '/index.css';
+    }
+
+    return `\
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <title>playground</title>${env === 'development' ? `
+        <script>
+            new EventSource('/esbuild').addEventListener('change', () => {
+                location.reload();
+            });
+        </script>` : ''}
+        <link rel="stylesheet" type="text/css" href="${csspath}"/>
+    </head>
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="app"></div>
+        <script src="${jspath}"></script>
+    </body>
+</html>
+`;
+}
+
+module.exports = {generateHTML};
+exports.default = {generateHTML};
